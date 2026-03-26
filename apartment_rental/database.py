d13a9e037a71a1f44a57f3aa8336e6b9f0f462c2
Myk0laVasylenk0 <143267565+Myk0laVasylenk0@users.noreply.github.com>
2026-03-26 13:15:32 +0200

    add apartment_renta

---
+++ apartment_rental/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./rental.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+Base = declarative_base()
